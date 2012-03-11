f4123ae0a169cedbce8d127f35ee76285d0b7081
Bruno Santos <wyldckat@gmail.com>
2012-03-11 17:36:11 +0000

    Liquido comprimido já funciona para 'p e/ou T'.

---
+++ ferramentas/extract2nspire.js
@@ -0,0 +1,18 @@
+{
+  "keyboard_shortcut": "", 
+  "name": "extract2nspire", 
+  "language": "JavaScript", 
+  "trigger_enabled": false, 
+  "value": [
+    "// Macro recorded on: Sat Mar 10 2012 19:13:32 GMT+0000 (GMT Standard Time)", 
+    "komodo.assertMacroVersion(3);", 
+    "if (komodo.view) { komodo.view.setFocus(); }", 
+    "ko.commands.doCommand('cmd_documentHome')", 
+    "ko.commands.doCommand('cmd_selectLineNext')", 
+    "ko.commands.doCommand('cmd_selectLineNext')", 
+    "ko.commands.doCommand('cmd_delete')", 
+    "ko.commands.doCommand('cmd_documentEnd')", 
+    "ko.commands.doCommand('cmd_selectLinePrevious')", 
+    "ko.commands.doCommand('cmd_delete')", 
+    "ko.commands.doCommand('cmd_documentHome')", 
+    
